4386641479522c3936dc6a75f6e28a6d71e5d461
DoksaVPC <55451950+DoksaVPC@users.noreply.github.com>
2021-01-14 04:27:28 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
+const wrapper = document.getElementById("wrapper");
 const handler = document.getElementById("player-handler");
+const controller = document.getElementById("controller");
 const handlerSize = 30;
+const controllerSize = 40;
 
 const player1 = document.getElementById("player-1");
 const player2 = document.getElementById("player-2");
@@ -7,21 +10,52 @@ const player3 = document.getElementById("player-3");
 const player4 = document.getElementById("player-4");
 const player5 = document.getElementById("player-5");
 const player6 = document.getElementById("player-6");
+const immagine = document.getElementById("immagine");
+const video = document.getElementById("video");
 
-const width = document.documentElement.clientWidth;
-const height = document.documentElement.clientHeight;
-const handlerXperc = handlerSize/2 * 100 / width;
-const handlerYperc = handlerSize/2 * 100 / height;
-const minY = 20;
-const maxY = 80 - handlerYperc;
+
+let width = document.documentElement.clientWidth;
+let height = document.documentElement.clientHeight;
+
+let handlerXperc = handlerSize/2 * 100 / width;
+let handlerYperc = handlerSize/2 * 100 / height;
+let controllerPerc = controllerSize * 100 / height;
+let wrapperwidth = (height - controllerSize) / 9 * 16;
+let wrapperwidthPerc = wrapperwidth * 100 / width;
+const minY = 25;
+const maxY = (height - controllerSize) / 3 * 2 * 100 / height;
 const minX = 20;
-const maxX = 80 - handlerXperc;
+const maxX = 60;
 let isDragging = false;
 
+const barWidth = (100 - wrapperwidthPerc) / 3;
+const bar1 = document.getElementById("guideline-2");
+bar1.style.width = barWidth + 'vw';
+const bar2 = document.getElementById("guideline-3");
+bar2.style.width = barWidth + 'vw';
+const bar3 = document.getElementById("guideline-4");
+bar3.style.width = barWidth + 'vw';
+bar1.style.left = 100 - barWidth * 3 + 'vw';
+bar2.style.left = 100 - barWidth * 2 + 'vw';
+bar3.style.left = 100 - barWidth + 'vw';
+
+immagine.style.left = minX/2 + 'vw';
+immagine.style.top = minY + 'vh';
+immagine.style.width = maxX - minX /4 + 'vw';
+video.style.minWidth = wrapperwidthPerc / 3 * 2 + handlerXperc + 'vw';
+
+wrapper.style.width = wrapperwidthPerc + '%';
+wrapper.style.height = (height - controllerSize) * 100 / height + 'vh';
 handler.style.width = handlerSize + 'px';
 handler.style.height = handlerSize + 'px';
+controller.style.height = controllerSize + 'px';
 handler.style.top = (height/2 - handlerSize/2) * 100 / height + '%';
 handler.style.left = (width/2 - handlerSize/2) * 100 / width + '%';
+
+
+setPositionX(wrapperwidthPerc / 3 * 2);
+setPositionY((height - controllerSize) / 3 * 2 * 100 / height);
+
 
 document.onmousemove = moveHandler;
 
@@ -40,30 +74,11 @@ function moveHandler(mouseEvent){
     mouseX = (mouseEvent.pageX - handlerSize/2) * 100 / width;
     mouseY = (mouseEvent.pageY - handlerSize/2) * 100 / height;
     if (mouseX >= minX && mouseX <= maxX){
-      handler.style.left = mouseX + '%';
-      player1.style.width = mouseX + handlerXperc + '%';
-      player2.style.width = 100 - (mouseX + handlerXperc) + '%';
-      player2.style.left = mouseX + handlerXperc + '%';
-      player3.style.width = 100 - (mouseX + handlerXperc) + '%';
-      player3.style.left = mouseX + handlerXperc + '%';
-      player4.style.width = (mouseX + handlerXperc) / 2 + '%';
-      player5.style.width = (mouseX + handlerXperc) / 2 + '%';
-      player5.style.left = (mouseX + handlerXperc) / 2 + '%';
-      player6.style.width = 100 - (mouseX + handlerXperc) + '%';
-      player6.style.left = mouseX + handlerXperc + '%';
+      setPositionX(mouseX);
+
     }
     if (mouseY >= minY && mouseY <= maxY){
-      handler.style.top = mouseY + '%';
-      player1.style.height = mouseY + handlerYperc + '%';
-      player2.style.height = (mouseY + handlerYperc) / 2 + '%';
-      player3.style.height = (mouseY + handlerYperc) / 2 + '%';
-      player3.style.top = (mouseY + handlerYperc) / 2 + '%';
-      player4.style.height = 100 - (mouseY + handlerYperc) + '%';
-      player4.style.top = mouseY + handlerYperc + '%';
-      player5.style.height = 100 - (mouseY + handlerYperc) + '%';
-      player5.style.top = mouseY + handlerYperc + '%';
-      player6.style.height = 100 - (mouseY + handlerYperc) + '%';
-      player6.style.top = mouseY + handlerYperc + '%';
+      setPositionY(mouseY);
     }
   }
 }
@@ -76,4 +91,33 @@ function handleDrag(){
     isDragging = false;
     console.log('released');
   }
+}
+
+function setPositionX(x){
+  handler.style.left = x + 'vw';
+  player1.style.width = x + handlerXperc + 'vw';
+  player2.style.width = wrapperwidthPerc - (x + handlerXperc) + 'vw';
+  player2.style.left = x + handlerXperc + 'vw';
+  player3.style.width = wrapperwidthPerc - (x + handlerXperc) + 'vw';
+  player3.style.left = x + handlerXperc + 'vw';
+  player4.style.width = (x + handlerXperc) / 2 + 'vw';
+  player5.style.width = (x + handlerXperc) / 2 + 'vw';
+  player5.style.left = (x + handlerXperc) / 2 + 'vw';
+  player6.style.width = wrapperwidthPerc - (x + handlerXperc) + 'vw';
+  player6.style.left = x + handlerXperc + 'vw';
+  video.style.width = player1.offsetWidth + 'px';
+}
+
+function setPositionY(y){
+  handler.style.top = y + 'vh';
+  player1.style.height = y + handlerYperc + 'vh';
+  player2.style.height = (y + handlerYperc) / 2 + 'vh';
+  player3.style.height = (y + handlerYperc) / 2 + 'vh';
+  player3.style.top = (y + handlerYperc) / 2 + 'vh';
+  player4.style.height = 100 - (y + handlerYperc) - controllerPerc + 'vh';
+  player4.style.top = y + handlerYperc + 'vh';
+  player5.style.height = 100 - (y + handlerYperc) - controllerPerc + 'vh';
+  player5.style.top = y + handlerYperc + 'vh';
+  player6.style.height = 100 - (y + handlerYperc) - controllerPerc + 'vh';
+  player6.style.top = y + handlerYperc + 'vh';
 }
