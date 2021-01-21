5c93f756efeff20e9c258f9f643d62308c362585
DoksaVPC <55451950+DoksaVPC@users.noreply.github.com>
2021-01-21 17:41:20 +0100

    added videos and first draft of controller

--- the-hijackers-guide/src/components/grid.js
+++ the-hijackers-guide/src/components/grid.js
@@ -1,5 +1,4 @@
-import { Link, useLocation } from "react-router-dom";
-import {React, useState} from 'react';
+import { React, useState, useRef } from "react";
 
 function Grid(props) {
   let WindowSize = {
@@ -11,85 +10,217 @@ function Grid(props) {
   let gridWidth = ((WindowSize.y - playerControllerHeight) / 9) * 16;
   let gridHeight = WindowSize.y - playerControllerHeight;
   let gridLeft = -1;
-  const handlerRadius = 24;
+  const handlerRadius = 20;
 
-  const [handlerPos, setHandlerPos] = useState({x: gridWidth/3 * 2 - handlerRadius, y: gridHeight/3*2 - handlerRadius});
+  const [handlerPos, setHandlerPos] = useState({
+    x: (gridWidth / 3) * 2 - handlerRadius,
+    y: (gridHeight / 3) * 2 - handlerRadius
+  });
   const [movingHandler, setMovingHandler] = useState(false);
 
-  function changeColor(e, color){
-      e.target.style.backgroundColor = color;
+  const video1 = useRef(null);
+  const video2 = useRef(null);
+  const video3 = useRef(null);
+  const video4 = useRef(null);
+  const video5 = useRef(null);
+  const video6 = useRef(null);
+
+  let isPlaying = false;
+
+  function changeColor(e, color) {
+    e.target.style.backgroundColor = color;
   }
 
-
-
-  function toggleGrab(e){
-      e.preventDefault();
-    if (movingHandler){
-      setMovingHandler(false)
+  function toggleGrab(e) {
+    e.preventDefault();
+    if (movingHandler) {
+      setMovingHandler(false);
     } else {
-      setMovingHandler(true)
+      setMovingHandler(true);
     }
   }
 
-  function handleMouseMove(e){
-    if(movingHandler){
-      if (gridLeft === -1){
-        gridLeft = document.getElementById('grid').getBoundingClientRect().left;
+  function handleMouseMove(e) {
+    if (movingHandler) {
+      if (gridLeft === -1) {
+        gridLeft = document.getElementById("grid").getBoundingClientRect().left;
       }
-      let mousePos = {x: e.pageX - gridLeft - handlerRadius, y: e.pageY - handlerRadius}
+      let mousePos = {
+        x: e.pageX - gridLeft - handlerRadius,
+        y: e.pageY - handlerRadius
+      };
       setHandlerPos({
         x: mousePos.x,
         y: mousePos.y
-      })
+      });
     }
   }
 
-  let location = useLocation();
+  function playVideos() {
+    if (!isPlaying) {
+      video1.current.play();
+      video2.current.play();
+      video3.current.play();
+      video4.current.play();
+      video5.current.play();
+      video6.current.play();
+      isPlaying = true;
+    } else {
+      video1.current.pause();
+      video2.current.pause();
+      video3.current.pause();
+      video4.current.pause();
+      video5.current.pause();
+      video6.current.pause();
+      isPlaying = false;
+    }
+  }
 
   return (
-    <div className="flex-wrapper" onMouseUp={event => {
-      if(movingHandler){
-        toggleGrab(event);
-      }
-      }}>
+    <div
+      className="flex-wrapper"
+      onMouseUp={event => {
+        if (movingHandler) {
+          toggleGrab(event);
+        }
+      }}
+    >
       <div
         className="flex-wrapper fadeIn"
-        style={{ backgroundColor: props.color, flexDirection: 'column'}}
+        style={{ backgroundColor: props.color, flexDirection: "column" }}
       >
         <div
           className="grid"
           id="grid"
           style={{
-            width: gridWidth, height: gridHeight
+            width: gridWidth,
+            height: gridHeight
           }}
           onMouseMove={event => handleMouseMove(event)}
-
         >
-          <div className="grid-container" style={{ flexBasis: handlerPos.y + handlerRadius }}>
-            <div className="grid-unit" style={{ width: handlerPos.x + handlerRadius }} />
-            <div className="double-container vertical" style={{ width: gridWidth - (handlerPos.x + handlerRadius) }}>
-              <div className="grid-unit half"></div>
-              <div className="grid-unit half"></div>
+          <div
+            className="grid-container"
+            style={{ height: handlerPos.y + handlerRadius }}
+          >
+            <div
+              className="grid-unit"
+              style={{ width: handlerPos.x + handlerRadius }}
+            >
+              <video ref={video1}>
+                <source
+                  src={
+                    "/assets/section" +
+                    props.sectionId +
+                    "/Video" +
+                    props.sectionId +
+                    "-Box1.mp4"
+                  }
+                />
+              </video>
+            </div>
+            <div
+              className="double-container vertical"
+              style={{ width: gridWidth - (handlerPos.x + handlerRadius) }}
+            >
+              <div className="grid-unit half">
+                <video ref={video2}>
+                  <source
+                    src={
+                      "/assets/section" +
+                      props.sectionId +
+                      "/Video" +
+                      props.sectionId +
+                      "-Box2.mp4"
+                    }
+                  />
+                </video>
+              </div>
+              <div className="grid-unit half">
+                <video ref={video3}>
+                  <source
+                    src={
+                      "/assets/section" +
+                      props.sectionId +
+                      "/Video" +
+                      props.sectionId +
+                      "-Box3.mp4"
+                    }
+                  />
+                </video>
+              </div>
             </div>
           </div>
-          <div className="grid-container" style={{ flexBasis: gridHeight - (handlerPos.y + handlerRadius), borderBottom: 0 }}>
+          <div
+            className="grid-container"
+            style={{
+              height: gridHeight - (handlerPos.y + handlerRadius),
+              borderBottom: 0
+            }}
+          >
             <div
               className="double-container horizontal"
               style={{ width: handlerPos.x + handlerRadius }}
             >
-              <div className="grid-unit half"></div>
-              <div className="grid-unit half"></div>
+              <div className="grid-unit half">
+                <video ref={video4}>
+                  <source
+                    src={
+                      "/assets/section" +
+                      props.sectionId +
+                      "/Video" +
+                      props.sectionId +
+                      "-Box4.mp4"
+                    }
+                  />
+                </video>
+              </div>
+              <div className="grid-unit half">
+                <video ref={video5}>
+                  <source
+                    src={
+                      "/assets/section" +
+                      props.sectionId +
+                      "/Video" +
+                      props.sectionId +
+                      "-Box5.mp4"
+                    }
+                  />
+                </video>
+              </div>
             </div>
-            <div className="grid-unit" style={{ width: gridWidth - (handlerPos.x + handlerRadius) }} />
+            <div
+              className="grid-unit"
+              style={{ width: gridWidth - (handlerPos.x + handlerRadius) }}
+            >
+              <video ref={video6}>
+                <source
+                  src={
+                    "/assets/section" +
+                    props.sectionId +
+                    "/Video" +
+                    props.sectionId +
+                    "-Box6.mp4"
+                  }
+                />
+              </video>
+            </div>
           </div>
-          <div className="handler" style={{left: handlerPos.x, top: handlerPos.y}}
-           onMouseDown={toggleGrab}
-           onMouseEnter={event => changeColor(event, props.color)}
-           onMouseOut={event => changeColor(event, 'GhostWhite')}/>
+          <div
+            className="handler"
+            style={{ left: handlerPos.x, top: handlerPos.y }}
+            onMouseDown={toggleGrab}
+            onMouseEnter={event => changeColor(event, props.color)}
+            onMouseOut={event => changeColor(event, "GhostWhite")}
+          />
         </div>
-        <div className="controller-container" style={{height: playerControllerHeight, width: gridWidth}}>
-        <div className="controller-slider"/>
-        <div className="controller-button"><img src="/assets/play_button.svg" alt="play"/></div>
+        <div
+          className="controller-container"
+          style={{ height: playerControllerHeight, width: gridWidth }}
+        >
+          <div className="controller-slider" />
+          <div className="controller-button" onClick={playVideos}>
+            <img src="/assets/play_button.svg" alt="play" />
+          </div>
         </div>
       </div>
     </div>
--- the-hijackers-guide/src/components/section.js
+++ the-hijackers-guide/src/components/section.js
@@ -1,32 +1,30 @@
-import {Link, useLocation} from "react-router-dom";
-import Grid from './grid'
+import { Link, useLocation } from "react-router-dom";
+import Grid from "./grid";
 
+function Section(props) {
+  let location = useLocation();
 
-function Section(props){
+  const COLORS = {
+    "01": "#CD7FF2",
+    "02": "#1EDA96",
+    "03": "#FFAF14",
+    "04": "#3BB8FF",
+    black: "#1C0F13"
+  };
 
-let location = useLocation();
+  const DARK_COLORS = {
+    "01": "#a05ebf",
+    "02": "#16a873",
+    "03": "#c2840e",
+    "04": "#3BB8FF"
+  };
 
-const COLORS = {
-  '01': '#CD7FF2',
-  '02': '#1EDA96',
-  '03': '#FFAF14',
-  '04': '#3BB8FF',
-  'black': '#1C0F13'
-}
-
-const DARK_COLORS = {
-  '01': '#a05ebf',
-  '02': '#16a873',
-  '03': '#c2840e',
-  '04': '#3BB8FF',
-}
-
-const TITLES = {
-  '01': 'Hashtag',
-  '02': 'Social Media',
-  '03': 'The Perfect Moment',
-  '04': 'Hijacking Tactics'
-}
+  const TITLES = {
+    "01": "Efficient hashtag",
+    "02": "Proper social media",
+    "03": "Perfect moment",
+    "04": "Effective tactic"
+  };
 
   return (
     <div
@@ -41,21 +39,50 @@ const TITLES = {
       {location.pathname !== "/guidelines/guideline-" + props.sectionId ? (
         <Link to={"/guidelines/guideline-" + props.sectionId}>
           <div className="preview-video-container">
-          {location.pathname === "/guidelines" && (
-            <div>
-            <video loop className="preview-video" onMouseOver={event => event.target.play()}
-  onMouseOut={event => event.target.pause()}><source src={"./assets/preview-" + props.sectionId + ".mp4"}/></video>
-          <div className="duotone duotone-dark" style={{backgroundColor: DARK_COLORS[props.sectionId]}}/>
-          <div className="duotone duotone-light" style={{backgroundColor: COLORS[props.sectionId]}}/>
-          </div>
-          )}
-
+            {location.pathname === "/guidelines" && (
+              <div>
+                <video
+                  loop
+                  className="preview-video"
+                  onMouseOver={event => event.target.play()}
+                  onMouseOut={event => event.target.pause()}
+                >
+                  <source
+                    src={"./assets/preview-" + props.sectionId + ".mp4"}
+                  />
+                </video>
+                <div
+                  className="duotone duotone-dark"
+                  style={{ backgroundColor: DARK_COLORS[props.sectionId] }}
+                />
+                <div
+                  className="duotone duotone-light"
+                  style={{ backgroundColor: COLORS[props.sectionId] }}
+                />
+              </div>
+            )}
           </div>
         </Link>
-      ) : (<Grid color={COLORS[props.sectionId]}/>) }
-      {location.pathname === "/guidelines" && (
+      ) : (
+        <Grid color={COLORS[props.sectionId]} sectionId={props.sectionId} />
+      )}
+      {location.pathname !== "/guidelines/guideline-" + props.sectionId && (
         <div className="sectionTitle-container">
-        <h2 className="sectionTitle-title">{TITLES[props.sectionId]}</h2>
+          <h2
+            className="sectionTitle-title"
+            style={
+              location.pathname !== "/guidelines"
+                ? {
+                    textOrientation: "mixed",
+                    writingMode: "vertical-lr",
+                    fontSize: "1vw",
+                    transform: "rotate(180deg)"
+                  }
+                : { undefined }
+            }
+          >
+            {TITLES[props.sectionId]}
+          </h2>
         </div>
       )}
     </div>
